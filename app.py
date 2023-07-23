456397ff8649e68ed05662e4f70b9cf37c40be29
Raman Baliyan <125754057+RamanBaliyan@users.noreply.github.com>
2023-07-23 10:05:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -242,7 +242,7 @@ if (selected == "Parkinsons Prediction"):
           parkinsons_diagnosis = "The person does not have Parkinson's disease"
         
     st.success(parkinsons_diagnosis)
-st.header(Created by Raman Baliyan :flag-in:)
+st.header("Created by Raman Baliyan :flag-in:")
 
 
 
